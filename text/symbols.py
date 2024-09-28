8404f2bedd5bc03c96c4b920a8611a687a6b454c
marcevrard <marc.evrard@gmail.com>
2024-09-28 13:39:11 +0200

    Fix for testing + cleaning

--- text/symbols.py
+++ text/symbols.py
@@ -3,14 +3,12 @@
 '''
 Defines the set of symbols used in text input to the model.
 '''
-_pad        = '_'
-_punctuation = ';:,.!?¡¿—…"«»“” '
-_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-_letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
-
+_punctuation = '"'
+_full_alphabet = " !*&'()+,-./0123456789:;?ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz|«°»ÀÂÇÈÉÊÎÏÔÙÛàâæçèéêëîïôöùúûüœ–’“”…ÅåûPRSTVWYZ"
 
 # Export all symbols:
-symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
+symbols = [_punctuation] + list(_full_alphabet)
+
 
 # Special symbol ids
 SPACE_ID = symbols.index(" ")
