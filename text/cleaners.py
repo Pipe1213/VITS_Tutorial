075e58cf2b185f6735d3ab6669b2a286899c37f2
Pipe1213 <123564842+Pipe1213@users.noreply.github.com>
2024-10-03 13:58:23 +0200

    Update cleaners.py

--- text/cleaners.py
+++ text/cleaners.py
@@ -20,81 +20,35 @@ from phonemizer import phonemize
 # Regular expression matching whitespace:
 _whitespace_re = re.compile(r'\s+')
 
-# List of (regular expression, replacement) pairs for abbreviations:
-_abbreviations = [(re.compile('\\b%s\\.' % x[0], re.IGNORECASE), x[1]) for x in [
-  ('mrs', 'misess'),
-  ('mr', 'mister'),
-  ('dr', 'doctor'),
-  ('st', 'saint'),
-  ('co', 'company'),
-  ('jr', 'junior'),
-  ('maj', 'major'),
-  ('gen', 'general'),
-  ('drs', 'doctors'),
-  ('rev', 'reverend'),
-  ('lt', 'lieutenant'),
-  ('hon', 'honorable'),
-  ('sgt', 'sergeant'),
-  ('capt', 'captain'),
-  ('esq', 'esquire'),
-  ('ltd', 'limited'),
-  ('col', 'colonel'),
-  ('ft', 'fort'),
-]]
-
-
-def expand_abbreviations(text):
-  for regex, replacement in _abbreviations:
-    text = re.sub(regex, replacement, text)
-  return text
-
-
-def expand_numbers(text):
-  return normalize_numbers(text)
-
-
 def lowercase(text):
   return text.lower()
-
 
 def collapse_whitespace(text):
   return re.sub(_whitespace_re, ' ', text)
 
-
-def convert_to_ascii(text):
-  return unidecode(text)
-
-
 def basic_cleaners(text):
   '''Basic pipeline that lowercases and collapses whitespace without transliteration.'''
-  text = lowercase(text)
+  #text = lowercase(text)
   text = collapse_whitespace(text)
+  text = replace_quote(text)
+  text = remove_special_characters(text)
+  text = remove_hyphen_at_start(text)
   return text
 
+def replace_quote(text):
+    return text.replace('’', "'")
 
-def transliteration_cleaners(text):
-  '''Pipeline for non-English text that transliterates to ASCII.'''
-  text = convert_to_ascii(text)
-  text = lowercase(text)
-  text = collapse_whitespace(text)
-  return text
+def remove_special_characters(text):
+    # Define the characters to remove
+    characters_to_remove = ['«', '»']
+    # Remove the characters from the text
+    for char in characters_to_remove:
+        text = text.replace(char, '')
+    return text
 
-
-def english_cleaners(text):
-  '''Pipeline for English text, including abbreviation expansion.'''
-  text = convert_to_ascii(text)
-  text = lowercase(text)
-  text = expand_abbreviations(text)
-  phonemes = phonemize(text, language='en-us', backend='espeak', strip=True)
-  phonemes = collapse_whitespace(phonemes)
-  return phonemes
-
-
-def english_cleaners2(text):
-  '''Pipeline for English text, including abbreviation expansion. + punctuation + stress'''
-  text = convert_to_ascii(text)
-  text = lowercase(text)
-  text = expand_abbreviations(text)
-  phonemes = phonemize(text, language='en-us', backend='espeak', strip=True, preserve_punctuation=True, with_stress=True)
-  phonemes = collapse_whitespace(phonemes)
-  return phonemes
+def remove_hyphen_at_start(text):
+    # Check if the text starts with '-'
+    if text.startswith('-'):
+        # Remove the hyphen at the start
+        text = text[1:].lstrip()
+    return text
